9712203f2f808b4b717f856a0040e743bfb36dff
Monika <gantamonika1998@gmail.com>
2021-12-06 17:00:51 +0530

    welcom-login commit

---
+++ 43-js-callbacks.js
@@ -0,0 +1,56 @@
+// // problems in asynchronous programming 
+// console.log("output 1"); // 1
+// const getFun = () => {
+//     setTimeout(() => {
+//         return { message: "fun meesage" }; // react to spring boot 
+//     }, 2000);
+// }
+// // TypeError: Cannot read property 'message' of undefined
+// const fun = getFun(); // 2, but delayed 
+// console.log(fun.message); //  3, fails because it depends on output  2
+
+// solution 1. - use callback 
+// console.log("output 1"); // 1 
+
+// const getFun = (abc) => {  // 3
+//     setTimeout(() => {
+//         abc({ message: "fun meesage" }); // callback  // 4
+//     }, 2000);
+// }
+// const funCall = (arg) => { // 5 
+//     console.log(arg.message);  // 6
+// }
+// // success 
+// getFun(funCall); // 2 
+
+// solution 2 -  use Promise  
+console.log("output 1"); // 1 
+
+const getFun = () => {  // 3
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const err = true; // true / false  
+            if (err)
+                reject();
+            else
+                resolve({ message: "fun meesage" });
+        }, 2000);
+    });
+}
+
+getFun().then((obj) => {
+    console.log(obj.message);
+}).catch(() => {
+    console.log("Error...");
+});
+
+// use in React 
+// const empData;
+// axios.get("http:localhost:8082/getemp") 
+//     .then((emp) => {
+//         empData = emp;
+//     })
+//     .catch((e) => {
+//         // 
+//     });
+
---
+++ 50-script.js
@@ -0,0 +1,16 @@
+// 51-login.html 
+loginFunction = () => {
+    if (document.getElementById("userName").value === "Sonu"
+        && document.getElementById("password").value === "Sonu") {
+        window.localStorage.setItem("user", document.getElementById("userName").value);
+        window.location = "./52-welcome.html";
+    }
+    else {
+        document.getElementById("credentials").innerHTML = "Invalid credentials!";
+        window.localStorage.removeItem("user");
+    }
+}
+
+// 52-welcome.html 
+document.getElementById("credentials").innerHTML = window.localStorage.getItem("user");
+window.localStorage.removeItem("user");
